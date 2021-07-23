fd781fc7fcbb1bdcfc79c2d13ed246afaccf19ec
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-07-23 12:11:42 -0600

    Starting over

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/globals.scss'
+import '../styles/reset.css';
+import '../styles/main.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import SmallCard from '../components/SmallCard';
+import { projectIcons } from '../components/Icons';
 
-import Layout from "../components/layout";
+import { projects } from '../utils/projectsData';
 
-const Home = () => {
-  // get user
-  useEffect(async () => {
-    const response = await fetch(`/api/user`);
-    console.log(response, "response");
-    // const json = await response.json()
-    // console.log(json, 'json')
-  });
-
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/reports/new");
-  });
-
-  return <Layout></Layout>;
-};
+const Home = () => (
+  <div className="home">
+    <h1>What Can I Deploy to Static Apps?</h1>
+    <div className="card-grid">
+      {projects.map((project) => {
+        const Icon = projectIcons[project.id];
+        return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
+      })}
+    </div>
+  </div>
+);
 
 export default Home;

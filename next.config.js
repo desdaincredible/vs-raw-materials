e85c111a1fe01921d7ec83b7a02a320f48c6292f
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-07-26 10:08:13 -0600

    Removed server side rendering

--- next.config.js
+++ next.config.js
@@ -1,30 +1,43 @@
-// const data = require("./utils/projectsData");
-const data = require("./utils/pages");
+// // const data = require("./utils/projectsData");
+// const data = require("./utils/pages");
+
+// module.exports = {
+//   trailingSlash: true,
+//   exportPathMap: async function () {
+//     // const { projects } = data;
+//     const { pages } = data;
+//     const paths = {
+//       "/": { page: "/" },
+//     };
+
+//     pages.forEach((page) => {
+//       paths[`/${page.slug}`] = {
+//         page: page.path ? `${page.locale}}/${page.path}` : `${page.locale}`,
+//         // page: "/page/[path]",
+//         // query: { path: page.slug },
+//       };
+//     });
+
+//     // pages.forEach((page) => {
+//     //   paths.push({
+//     //     page: page.path ? `/${page.locale}}/${page.path}` : `/${page.locale}`,
+//     //     query: { path: page.slug },
+//     //   });
+//     // });
+
+//     return paths;
+//   },
+// };
 
 module.exports = {
-  trailingSlash: true,
-  exportPathMap: async function () {
-    // const { projects } = data;
-    const { pages } = data;
-    const paths = {
-      "/": { page: "/" },
-    };
-
-    pages.forEach((page) => {
-      paths[`/${page.slug}`] = {
-        page: page.path ? `${page.locale}}/${page.path}` : `${page.locale}`,
-        // page: "/page/[path]",
-        // query: { path: page.slug },
-      };
-    });
-
-    // pages.forEach((page) => {
-    //   paths.push({
-    //     page: page.path ? `/${page.locale}}/${page.path}` : `/${page.locale}`,
-    //     query: { path: page.slug },
-    //   });
-    // });
-
-    return paths;
+  target: "serverless",
+  async rewrites() {
+    return [
+      // Rewrite everything to `pages/index`
+      {
+        source: "/:any*",
+        destination: "/",
+      },
+    ];
   },
 };
--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,20 @@ import "../styles/globals.scss";
 import "../styles/reset.css";
 import "../styles/main.css";
 
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SafeHydrate>
+      <Component {...pageProps} />
+    </SafeHydrate>
+  );
 }
 
 export default MyApp;

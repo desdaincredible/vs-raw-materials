9a7f9e758ad2733dee1b6a581320a7433a59d737
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-08-30 16:27:38 -0600

    Removed unnecessary line

--- app/pages/reports/new.js
+++ app/pages/reports/new.js
@@ -6,7 +6,6 @@ export default function ReportsNew() {
   return (
     <Layout>
       <h1>Reports</h1>
-      <h1>IS THIS WORKING?</h1>
 
       <strong className="text-uppercase">Upload Purchase Order</strong>
       <p className="small text-muted">exported from Rain</p>

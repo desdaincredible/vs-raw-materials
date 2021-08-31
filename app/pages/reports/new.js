d9373d35a9a96da5d6ac7887a45754e8e8e9b9e0
Destiny Hillis <destinyhillis@Destinys-MacBook-Pro.local>
2021-08-31 09:31:24 -0600

    Sample azure function working

--- app/pages/reports/new.js
+++ app/pages/reports/new.js
@@ -1,12 +1,25 @@
-import Layout from '../../components/layout'
-import Datepicker from '../../components/datepicker'
-import CSVReader from '../../components/csv-reader'
+import Layout from '../../components/layout';
+import Datepicker from '../../components/datepicker';
+import CSVReader from '../../components/csv-reader';
+import { Button } from 'reactstrap';
+import Link from 'next/link';
 
 export default function ReportsNew() {
+  const upload = async (e) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/uploadCSV`);
+    const data = await res.json();
+    console.log(res);
+
+    if (!data) {
+      return {
+        notFound: true,
+      };
+    }
+  };
+
   return (
     <Layout>
       <h1>Reports</h1>
-
       <strong className="text-uppercase">Upload Purchase Order</strong>
       <p className="small text-muted">exported from Rain</p>
 
@@ -14,6 +27,15 @@ export default function ReportsNew() {
         <Datepicker />
       </div>
       <CSVReader />
+      <Link href="http://localhost:3000/reports/new/">
+        <a
+          onClick={(e) => {
+            upload();
+          }}
+        >
+          <Button>Click Me</Button>
+        </a>
+      </Link>
     </Layout>
-  )
+  );
 }
